9dd9b3f36fbd4acc7274a17cabcc47c9d799f8e5
Jess Chadwick <jesschadwick@gmail.com>
2024-11-10 19:48:17 -0500

    Add GH Pages SPA support

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,4 +1,4 @@
-import { ScrollViewStyleReset } from 'expo-router/html';
+import { ScrollViewStyleReset } from "expo-router/html";
 
 // This file is web-only and used to configure the root HTML for every
 // web page during static rendering.
@@ -10,7 +10,33 @@ export default function Root({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        {`<script type="text/javascript">
+          // Single Page Apps for GitHub Pages
+          // MIT License
+          // https://github.com/rafgraph/spa-github-pages
+          // This script checks to see if a redirect is present in the query string,
+          // converts it back into the correct url and adds it to the
+          // browser's history using window.history.replaceState(...),
+          // which won't cause the browser to attempt to load the new url.
+          // When the single page app is loaded further down in this file,
+          // the correct url will be waiting in the browser's history for
+          // the single page app to route accordingly.
+          (function(l) {
+            if (l.search[1] === '/' ) {
+              var decoded = l.search.slice(1).split('&').map(function(s) { 
+                return s.replace(/~and~/g, '&')
+              }).join('?');
+              window.history.replaceState(null, null,
+                  l.pathname.slice(0, -1) + decoded + l.hash
+              );
+            }
+          }(window.location))
+        </script>`}
 
         {/* 
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native. 
